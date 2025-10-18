9a195785f8c318923eb10d2608ce9910fdc2fd3d
Matt Briers <matti.briers@gmail.com>
2025-10-18 16:41:56 +0200

    aabb collision uitzoeken

--- aabb_bvh.h
+++ aabb_bvh.h
@@ -11,28 +11,31 @@ using std::shared_ptr;
 
 class aabb_bvh : public hittable {
     public:
-        std::vector<shared_ptr<hittable>> objects;
+        shared_ptr<aabb_bvh> left, right;
 
         aabb_bvh(std::vector<shared_ptr<hittable>> objects) { 
+            // uuuh
             std::vector<shared_ptr<aabb_bvh>> boxes = ;
-         }
 
-        
+            this->left = aabb_bvh()
+        }
     
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             bool hit = false;
             hit_record temp_rec;
             double closest_so_far = ray_t.max;
 
-            for(const auto& object : objects) {
-                if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
-                    hit = true;
-                    closest_so_far = temp_rec.t;
-                    rec = temp_rec;
-                }
-                
+            if (left->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
+                hit = true;
+                closest_so_far = temp_rec.t;
             }
-            return hit;
+            
+            if (right->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
+                hit = true;
+                return right->hit(r, ray_t, rec);
+            }
+                
+            return left->hit(r, ray_t, rec);
         }
 };
 
