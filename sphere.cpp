5fbae0a5adaf2679de5693114ae38c7e86d5be5d
Matt Briers <matti.briers@gmail.com>
2025-10-28 15:41:22 +0100

    het compiled en ik wil de rest niet overzetten ik hoop dat dit mij later niet in mijn poep komt bijten

--- sphere.cpp
+++ sphere.cpp
@@ -1,6 +1,30 @@
 #include "sphere.h"
 #include "box.h"
 
+bool sphere::hit(const ray& r, interval ray_t, hit_record& rec) const {
+    vec3 oc = center - r.origin();
+    double a = r.direction().length_squared();
+    double h = dot(r.direction(), oc);
+    double c = oc.length_squared() - radius*radius;
+    double discriminant = h*h - a*c;
+    if (discriminant < 0) {
+        return false;
+    } 
+    double sqrtd = std::sqrt(discriminant);
+    double root = (h - sqrtd) / a;
+    if (!ray_t.surrounds(root)) {
+        root = (h + sqrtd) / a;
+        if (!ray_t.surrounds(root)) {
+            return false;
+        }
+    }
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+    return true;
+}
+
 box sphere::aabb() const {
     return box(
         point3(center.x() - radius, center.y() - radius, center.z() - radius),
--- sphere.h
+++ sphere.h
@@ -8,33 +8,7 @@ class sphere : public hittable {
     public:
         sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
 
-        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-            vec3 oc = center - r.origin();
-            double a = r.direction().length_squared();
-            double h = dot(r.direction(), oc);
-            double c = oc.length_squared() - radius*radius;
-
-            double discriminant = h*h - a*c;
-            if (discriminant < 0) {
-                return false;
-            } 
-            double sqrtd = std::sqrt(discriminant);
-
-            double root = (h - sqrtd) / a;
-            if (!ray_t.surrounds(root)) {
-                root = (h + sqrtd) / a;
-                if (!ray_t.surrounds(root)) {
-                    return false;
-                }
-            }
-
-            rec.t = root;
-            rec.p = r.at(rec.t);
-            vec3 outward_normal = (rec.p - center) / radius;
-            rec.set_face_normal(r, outward_normal);
-
-            return true;
-        }
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override;
 
         box aabb() const override;
 
