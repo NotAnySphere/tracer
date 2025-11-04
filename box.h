a82abf32fa3604c4bb211d0670c291b6b0a37a71
Matt Briers <matti.briers@gmail.com>
2025-11-04 10:29:10 +0100

    fixed box::compare and updated run.sh

--- box.h
+++ box.h
@@ -44,17 +44,17 @@ class box : public hittable {
             );
         }
 
-        static bool compare(const shared_ptr<box> a, const shared_ptr<box> b, int axis) {
-            return a->p1.e[axis] < b->p1.e[axis];
+        static bool compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+            return a->aabb().p1.e[axis] < b->aabb().p1.e[axis];
         }
 
-        static bool compare_x(const shared_ptr<box> a, const shared_ptr<box> b) {
+        static bool compare_x(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
             return compare(a, b, 0);
         }
-        static bool compare_y(const shared_ptr<box> a, const shared_ptr<box> b) {
+        static bool compare_y(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
             return compare(a, b, 1);
         }
-        static bool compare_z(const shared_ptr<box> a, const shared_ptr<box> b) {
+        static bool compare_z(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
             return compare(a, b, 2);
         }
 
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# cmake --fresh -DSDL_X11_XSCRNSAVER=OFF -B build
+
 cmake --build build -j=10
 
 ./build/first_image
